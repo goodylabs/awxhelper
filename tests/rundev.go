47f800a28a50f34fbc21aa60ea3287b569d9ecdc
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2025-09-08 11:14:11 +0200

    more info on fail

--- tests/rundev.go
+++ tests/rundev.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
-	"github.com/goodylabs/awxhelper/internal/adapters/awxconnector"
 	"github.com/goodylabs/awxhelper/internal/adapters/httpconnector"
 	"github.com/goodylabs/awxhelper/internal/ports"
 	"github.com/goodylabs/awxhelper/pkg/config"
@@ -18,39 +19,39 @@ func main() {
 	USERNAME := os.Getenv("AWX_USER")
 	PASSWORD := os.Getenv("AWX_PASSWORD")
 
-	connector := awxconnector.NewAwxConnector(httpconnector.NewHttpConnector())
-	if err := connector.ConfigureConnection(&ports.AwxConfig{
-		URL:      URL,
-		Username: USERNAME,
-		Password: PASSWORD,
-	}); err != nil {
-		log.Fatalf("failed to verify connection: %+v", err)
-	}
-	if _, err := connector.JobProgress(2804); err != nil {
-		log.Fatalf("failed to get job progress: %+v", err)
-	}
-
-	// PATH := "/api/v2/jobs/2741/job_events?page_size=100"
-	// connector := httpconnector.NewHttpConnector()
-	// respBody, statusCode, err := connector.DoGet(ports.HttpConnOpts{
-	// 	BaseURL:  URL,
+	// connector := awxconnector.NewAwxConnector(httpconnector.NewHttpConnector())
+	// if err := connector.ConfigureConnection(&ports.AwxConfig{
+	// 	URL:      URL,
 	// 	Username: USERNAME,
 	// 	Password: PASSWORD,
-	// }, PATH)
-	// if err != nil {
-	// 	log.Fatal("failed to get job progress: %", err)
+	// }); err != nil {
+	// 	log.Fatalf("failed to verify connection: %+v", err)
 	// }
-	// if statusCode != 200 {
-	// 	log.Fatalf("failed to get job progress, status %d", statusCode)
+	// if _, err := connector.JobProgress(2804); err != nil {
+	// 	log.Fatalf("failed to get job progress: %+v", err)
 	// }
-	// var prettyJSON map[string]any
-	// if err := json.Unmarshal(respBody, &prettyJSON); err != nil {
-	// 	log.Fatal("failed to unmarshal json: %", err)
-	// }
-	// var out []byte
-	// out, err = json.MarshalIndent(prettyJSON, "", "  ")
-	// if err != nil {
-	// 	log.Fatal("failed to marshal json: %", err)
-	// }
-	// fmt.Println(string(out))
+
+	PATH := "/api/v2/jobs/3185/job_events?page_size=100"
+	connector := httpconnector.NewHttpConnector()
+	respBody, statusCode, err := connector.DoGet(ports.HttpConnOpts{
+		BaseURL:  URL,
+		Username: USERNAME,
+		Password: PASSWORD,
+	}, PATH)
+	if err != nil {
+		log.Fatal("failed to get job progress: %", err)
+	}
+	if statusCode != 200 {
+		log.Fatalf("failed to get job progress, status %d", statusCode)
+	}
+	var prettyJSON map[string]any
+	if err := json.Unmarshal(respBody, &prettyJSON); err != nil {
+		log.Fatal("failed to unmarshal json: %", err)
+	}
+	var out []byte
+	out, err = json.MarshalIndent(prettyJSON, "", "  ")
+	if err != nil {
+		log.Fatal("failed to marshal json: %", err)
+	}
+	fmt.Println(string(out))
 }
